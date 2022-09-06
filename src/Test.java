813985759cfe141b5b8adbce7433904a75f42d2b
SX_7 <sn1.test.preria.2002@gmail.com>
2022-09-06 18:07:08 +0200

    1.4 done

--- src/Test.java
+++ src/Test.java
@@ -3,11 +3,16 @@ public class Test {
     public static void main(String[] args) {
         //Already feeling the boilerplate
         Scanner read = new Scanner(System.in);
-        
+        int leIntVar = 123;
+        boolean theTruth = false;
+        double bananaSplitChallenge = 56.01;
+        System.out.println("Here's some fun facts: " + leIntVar + theTruth + bananaSplitChallenge);
         System.out.println("Provide input, Sir/Madam?");
         String input = read.nextLine();
         //This might seem funny, but it actually is
         System.out.println("Big Cheese likes " + input);
-
+        leIntVar = Integer.valueOf(input);
+        //Actually, it can be done with Boolean, and Double too
+        System.out.println("Here's some less fun facts: " + leIntVar + theTruth + bananaSplitChallenge);
     }
 }
