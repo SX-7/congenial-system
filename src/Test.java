a652ef4a0cdf7ee566b2bcf5fca1832fb842c122
SX_7 <sn1.test.preria.2002@gmail.com>
2022-09-06 17:59:39 +0200

    1.3 done

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //Already feeling the boilerplate
+        Scanner read = new Scanner(System.in);
+        
+        System.out.println("Provide input, Sir/Madam?");
+        String input = read.nextLine();
         //This might seem funny, but it actually is
-        System.out.println("Big Cheese");
+        System.out.println("Big Cheese likes " + input);
+
     }
 }
