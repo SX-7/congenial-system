7dc81541d3624637a2fa81da6b20ecdb548a42ab
SX_7 <sn1.test.preria.2002@gmail.com>
2022-09-07 09:29:56 +0200

    Part of arrays no.1

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
+
 public class Test {
     public static void main(String[] args) {
-        // Temporairly looking for differing syntax - so no code rn
+        ArrayList<String> lista = new ArrayList<>();
+        lista.add("Element number one, but actually index 0");
+        //System.out.println(lista.get(1)); // This will actually crash lmao
+        lista.add("Index 1");
+        System.out.println(lista.get(0));
+        System.out.println(lista.get(1));
+    }
 }
