bd748276cfb56a6c870b4a89bd02d7aabf578cd1
SX_7 <sn1.test.preria.2002@gmail.com>
2022-09-07 10:47:15 +0200

    Previous commit might've had wrong terminology, anyways 3.2 done

--- src/Test.java
+++ src/Test.java
@@ -2,11 +2,44 @@ import java.util.ArrayList;
 
 public class Test {
     public static void main(String[] args) {
-        ArrayList<String> lista = new ArrayList<>();
-        lista.add("Element number one, but actually index 0");
-        //System.out.println(lista.get(1)); // This will actually crash lmao
-        lista.add("Index 1");
-        System.out.println(lista.get(0));
-        System.out.println(lista.get(1));
+        ArrayList<Integer> lista = new ArrayList<>();
+        lista.add(1);
+        lista.add(2);
+        lista.add(3);
+        lista.add(4);
+        print(lista);
+        lista.remove(2); // this will remove element with val of 3
+        print(lista);
+        lista.add(1);
+        lista.add(1);
+        lista.add(1);
+        lista.add(1);
+        lista.add(1); 
+        // Intriguing! Seems to store them as objects, and doesn't allow such duplicates
+        // As they're the same object with the same reference of smth
+        lista.add(4);
+        lista.remove(Integer.valueOf(4));
+        if(lista.contains(4)){
+            System.out.println("Something went wrong...");
+        } else {
+            System.out.println("All good :+1:, 4 was not found");
+        }
+        System.out.println(lavg(lista));
+    }
+    public static void print(ArrayList<Integer> list) {
+        for (Integer iter : list) {
+            System.out.println(iter);
+        }
+    }
+    public static double lavg(ArrayList<Integer> list) {
+        int sum = 0;
+        if(list.size()>0){
+            for (Integer iter : list) {
+                sum+=iter;
+            }
+            return (double) sum / list.size();
+        } else {
+            return -1;
+        }
     }
 }
