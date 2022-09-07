d3bd3e28aa8c43c95e0cf45c575282e40616f1e3
SX_7 <sn1.test.preria.2002@gmail.com>
2022-09-07 18:39:25 +0200

    4-3 done

--- src/Test.java
+++ src/Test.java
@@ -1,39 +1,32 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Test {
     public static void main(String[] args) {
-        int[] lista = new int[10];
-        lista[0]=1;
-        lista[1]=2;
-        lista[2]=3;
-        lista[3]=4;
-        print(lista);
-        lista[4]=5;
-        lista[5]=1;
-        lista[6]=1;
-        lista[7]=1;
-        lista[8]=1; 
-        print(lista);
-        //lista.remove(Integer.valueOf(4));
-        //if(lista.contains(4)){
-        //    System.out.println("Something went wrong...");
-        //} else {
-        //    System.out.println("All good :+1:, 4 was not found");
-        //} // no such fun as easily with the default list
-        System.out.println(lavg(lista));
-    }
-    public static void print(int[] list) {
-        for (int iter : list) {
-            System.out.println(iter);
+        ArrayList<Person> home = new ArrayList<>();
+        home.add(new Person("BigCheese", 9999));
+        home.add(new Person("SmolCheese", 68));
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input name and age seperated by ',' pass nothing to end");
+        while (true) {
+            String in = input.nextLine();
+            if (!(in.length() == 0)) {
+                if (!(in.contains(","))) {
+                    continue;
+                } else {
+                    String[] res = in.split(",");
+                    home.add(new Person(res[0], Integer.valueOf(res[1])));
+                }
+            } else {
+                print(home);
+                break;
+            }
         }
     }
-    public static double lavg(int[] list) {
-        int sum = 0;
-        if(list.length>0){
-            for (Integer iter : list) {
-                sum+=iter;
-            }
-            return (double) sum / list.length;
-        } else {
-            return -1;
+
+    public static void print(ArrayList<Person> ppl){
+        for (Person per : ppl) {
+            System.out.println(per.getName()+" aged "+per.getAge());
         }
     }
 }
