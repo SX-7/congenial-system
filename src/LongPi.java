ab7ed058e344474cea40eb4842edcd1bbed9c247
SX_7 <sn1.test.preria.2002@gmail.com>
2022-09-11 19:36:02 +0200

    Added some more testing framework

--- src/LongPi.java
+++ src/LongPi.java
@@ -1,4 +1,26 @@
+import java.math.BigDecimal;
+
 public class LongPi{
-    
+    //sane default
+    BigDecimal pi = new BigDecimal(3);
+    public LongPi() {
+
+    }
+    public LongPi(int amount){
+        this.generate(amount);
+    }
+
+    public void generate(int amount){
+        this.pi = BigDecimal.valueOf(80085);
+    }
+
+    public BigDecimal get(int amount){
+        this.generate(amount);
+        return this.pi;
+    }
+
+    public BigDecimal get() {
+        return this.pi;
+    }
     
 }
